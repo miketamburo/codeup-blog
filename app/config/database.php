51823080d811b84eb2f9686d812b606ec06d2001
miketamburo <matsatam@gmail.com>
2014-03-26 11:30:22 -0500

    database migration lesson

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
-			'password'  => '',
+			'database'  => 'blog_db',
+			'username'  => 'mike',
+			'password'  => 'password',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
