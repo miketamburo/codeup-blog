56f23880b75cc678ae7109cca48578aec48246e6
miketamburo <matsatam@gmail.com>
2014-03-28 15:36:42 -0500

    added database and pagoda box files

--- app/config/database.php
+++ app/config/database.php
@@ -53,14 +53,14 @@ return array(
 		),
 
 		'mysql' => array(
-			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'blog_db',
-			'username'  => 'mike',
-			'password'  => 'password',
-			'charset'   => 'utf8',
-			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
+		    'driver'    => 'mysql',
+		    'host'      => isset($_SERVER['DB1_HOST']) ? $_SERVER['DB1_HOST'] : 'localhost',
+		    'database'  => isset($_SERVER['DB1_NAME']) ? $_SERVER['DB1_NAME'] : 'blog_db',
+		    'username'  => isset($_SERVER['DB1_USER']) ? $_SERVER['DB1_USER'] : 'mike',
+		    'password'  => isset($_SERVER['DB1_PASS']) ? $_SERVER['DB1_PASS'] : 'password',
+		    'charset'   => 'utf8',
+		    'collation' => 'utf8_unicode_ci',
+		    'prefix'    => '',
 		),
 
 		'pgsql' => array(
