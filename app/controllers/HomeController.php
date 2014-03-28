83ce67a4a4d9a5aaed9efac3f384c5166d811067
miketamburo <matsatam@gmail.com>
2014-03-28 16:31:23 -0500

    added showWelcome page

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -14,6 +14,9 @@ class HomeController extends BaseController {
 	|	Route::get('/', 'HomeController@showWelcome');
 	|
 	*/
+	public function showWelcome(){
+		return View::make('hello');
+	}
 
 	public function showResume(){
 		return View::make('resume');
